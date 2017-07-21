4996623d2dd1584b4fdf8f988359e729bc9b971a
Cristiano Betta <cristiano@betta.io>
2017-07-21 15:59:02 +0100

    Update extra_5_send_individual_fax_dotnet_35.cs

--- extra_5_send_individual_fax_dotnet_35.cs
+++ extra_5_send_individual_fax_dotnet_35.cs
@@ -5,46 +5,46 @@ using System.Text;
 
 namespace Interfax_35_sample
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+  class Program
+  {
+    static void Main(string[] args)
+    {
       // Your credentials
-			const string username = "username";
-			const string password = "password";
+      const string username = "username";
+      const string password = "password";
       
       // The fax you want to send
-			const string faxFilePath = "fax.pdf";
+      const string faxFilePath = "fax.pdf";
 
       // The API method to call
-			const string baseUri = "https://rest.interfax.net";
-			string uploadUri = "/outbound/faxes";
+      const string baseUri = "https://rest.interfax.net";
+      string uploadUri = "/outbound/faxes";
 
-			// Set the parameters as per
-			// https://www.interfax.net/en/dev/rest/reference/2918
-			var options = new Dictionary<String, String>(){
-				{"faxNumber", "+11111111112"}
-			};
+      // Set the parameters as per
+      // https://www.interfax.net/en/dev/rest/reference/2918
+      var options = new Dictionary<String, String>(){
+        {"faxNumber", "+11111111112"}
+      };
 
-			// Create a new web client for making API calls
-			WebClient webClient = new WebClient();
-		
-			// Build the URL with the options defined earlier
-			uploadUri += "?";
-			foreach (var x in options.Keys)
-			{
-				uploadUri += x + '=' + options[x] + '&';
-			}
+      // Create a new web client for making API calls
+      WebClient webClient = new WebClient();
+    
+      // Build the URL with the options defined earlier
+      uploadUri += "?";
+      foreach (var x in options.Keys)
+      {
+        uploadUri += x + '=' + options[x] + '&';
+      }
 
-			// Add the credentials to the headers
-			var encodedAuth = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
-			webClient.BaseAddress = baseUri;
-			webClient.Headers.Add(HttpRequestHeader.Authorization, "Basic " + encodedAuth);
-			webClient.Headers.Add(HttpRequestHeader.Accept, "application/json");
-			webClient.Headers.Add(HttpRequestHeader.ContentType, "application/pdf");
+      // Add the credentials to the headers
+      var encodedAuth = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+      webClient.BaseAddress = baseUri;
+      webClient.Headers.Add(HttpRequestHeader.Authorization, "Basic " + encodedAuth);
+      webClient.Headers.Add(HttpRequestHeader.Accept, "application/json");
+      webClient.Headers.Add(HttpRequestHeader.ContentType, "application/pdf");
 
-			// Submit the file
-			var result = webClient.UploadFile(uploadUri, "post", faxFilePath);
-		}
-	}
+      // Submit the file
+      var result = webClient.UploadFile(uploadUri, "post", faxFilePath);
+    }
+  }
 }
